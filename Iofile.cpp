e4c59919f05e4e7581c9b42b5c9a14f5ca8e9789
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-06-10 14:54:57 +0800

    memasukkan dan menulis nilai dari 'baris' kedalam file

--- Iofile.cpp
+++ Iofile.cpp
@@ -20,6 +20,8 @@ int main() {
         // loop akan berhenti jika anda masukkan karakter q
         if (baris == "q")
             break;
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
     }
 
 
