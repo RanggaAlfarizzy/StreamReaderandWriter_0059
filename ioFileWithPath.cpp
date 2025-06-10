1e24d2f3d01a8af49e3011ba56e2aa64443f7657
RanggaAlfarizzy <rangga.alfarizzy@gmail.com>
2025-06-10 15:32:50 +0800

    menulis input pengguna ke file hingga mengetik 'q' sebagai perintah untuk menghentikannya, dan menutup file nya

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -14,8 +14,19 @@ int main()
     // membuka File dalam mode menulis
     ofstream outfile;
     //menunjuk ke sebuah nama file
-    outfile.open(NamaFile + ".txt");
+    outfile.open(NamaFile + ".txt"), ios::out;
     cout << ">=menulis file, \'q\' untuk keluar" << endl;
-
-
+    //unlimited loop untuk menulis
+    while (true)
+    {
+        cout << "_ ";
+        // mendapatkan setiap karakter dalam satu garis
+        getline(cin, baris);
+        // loop akan berhenti jika anda masukkan karakter q
+        if (baris == "q") break;
+        // menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    // selesai dalam menulis sekarang tutup file nya
+    outfile.close();
 }
